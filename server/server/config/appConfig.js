ef0aee11d330054b4129bd6551efa77372ee0ef0
spaceadh <spacesealadhesives13@gmail.com>
2024-05-31 20:43:29 +0300

    hello

--- server/server/config/appConfig.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-export const appConfig = {
-  corsConfig: {
-    origin: ["*"], // Allow all origins during development
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allow all methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Include other headers as needed
-  },
-};
